cbdb184d97c44059528b8446431b05f27b036b6f
ahmedj47 <jabrielahmed96@gmail.com>
2018-03-02 13:57:37 -0600

    Not Working but a start

--- app/src/main/java/com/example/krohn/lab2completed/MainActivity.java
+++ app/src/main/java/com/example/krohn/lab2completed/MainActivity.java
@@ -9,12 +9,23 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
+        Resources res = getResources();
+        TypedArray icons = res.obtainTypedArray(R.array.icons);
+        Drawable drawable = icons.getDrawable(0);
+
+        TypedArray colors = res.obtainTypedArray(R.array.colors);
+        int color = colors.getColor(0,0);
         if(getResources().getConfiguration().orientation==Configuration.ORIENTATION_LANDSCAPE) {
             setContentView(R.layout.activity_landscape);
         } else{
             setContentView(R.layout.activity_portrait);
         }
+
+
+
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
     }
 }
